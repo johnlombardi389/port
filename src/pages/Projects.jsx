84d1fdbb14dab0934ccafc2843a3ff7ea735c0d3
johnlombardi389 <lombardij38@gmail.com>
2022-12-01 20:33:17 -0500

    Began setting up project cards grid

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="projects-grid">
+      <div className="card">
+        <h2>Project</h2>
+      </div>
+
+      <div className="card">
+        <h2>Project</h2>
+      </div>
+
+      <div className="card">
+        <h2>Project</h2>
+      </div>
+
+      <div className="card">
+        <h2>Project</h2>
+      </div>
     </div>
   );
 }
