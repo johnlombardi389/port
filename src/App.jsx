4c6b34e1e5cc7642bb11e7190cc92f291c47ae92
johnlombardi389 <lombardij38@gmail.com>
2022-12-01 20:13:03 -0500

    Added all pages to app and added framer-motion animate presence to animate pages when changing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Routes, Route, useLocation } from "react-router-dom";
+// Animation
+import { AnimatePresence } from "framer-motion";
 // Styles
 import "./styles/app.scss";
 // Component
 import Nav from "./components/Nav";
 // Pages
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const location = useLocation();
 
   return (
     <div className="App">
       <Nav />
-      <Home />
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
